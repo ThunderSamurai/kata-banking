1d9dbd2effc84434188bdafe25eec0cef0750341
MANSOURI Mohamed Moez <mansourimedmoez@gmail.com>
2018-11-26 23:46:44 +0100

    on going implementation

---
+++ src/main/java/com/mmm/corebanking/shared/entities/MockProfiles.java
@@ -0,0 +1,7 @@
+package com.mmm.corebanking.shared.entities;
+
+public class MockProfiles {
+
+    public static final String MOCK_ACCOUNT_DAO="MOCK_ACCOUNT_DAO";
+    public static final String ACCOUNT_DATABASE_DAO = "ACCOUNT_DATABASE_DAO";
+}
